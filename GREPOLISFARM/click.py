94208353aec23672307b088246fa8ad007cd363b
Ivan13s <95711576+Ivan13s@users.noreply.github.com>
2022-11-07 15:03:00 +0200

    asdasdsad

--- GREPOLISFARM/click.py
+++ GREPOLISFARM/click.py
@@ -8,7 +8,12 @@ import tkinter
 import tkinter as tk
 from tkinter import *
 from threading import Thread, Event
+from tkinter import ttk
+import pandas as pd
+import numpy as np
+from time import sleep
 from termcolor import colored
+from tqdm import tqdm, trange
 import pyautogui
 
 root = Tk()
@@ -21,11 +26,11 @@ class Clicker:
 
     def alive_set(self):
         self.alive = True
-        print(20 * '-', 'Started', '_' * 20)
+        print(20 * '-', 'Start farm', '_' * 20)
 
     def alive_clean(self):
         self.alive = False
-        print(20 * '-', 'Stopped', '_' * 20)
+        print(20 * '-', 'Stop farm', '_' * 20)
 
     def alive_parade(self):
         self.paradis = True
@@ -36,14 +41,26 @@ class Clicker:
         print(20 * '-', 'Stopped parada cu orfeu', '_' * 20)
 
     def run(self):
+        i = 0
         while True:
             if self.alive:
-                time.sleep(1)
-                CEVA = random.uniform(0, 2)
+                for i in range(600):
+                    if self.alive:
+                        my_progress.start(interval=6000)
+                        i += 1
+                        print(i)
+                        sleep(1)
+                    if self.alive is False or i == 600:
+                        my_progress.stop()
+
+                print("Good")
+                CEVA = random.uniform(2, 5)
                 time.sleep(CEVA)
                 print("FARM: ", CEVA)
-                pyautogui.leftClick(random.uniform(813, 818), random.uniform(328, 334),interval=random.uniform(0.2, 0.9),duration=random.uniform(0.2, 0.9))  # for selel
-                pyautogui.leftClick(random.uniform(1220, 1260), random.uniform(798, 803),interval=random.uniform(0.2, 0.9), duration=random.uniform(0.2, 0.9))  # for collect
+                pyautogui.leftClick(random.uniform(813, 818), random.uniform(328, 334),
+                                    interval=random.uniform(0.2, 0.9), duration=random.uniform(0.2, 0.9))  # for selel
+                pyautogui.leftClick(random.uniform(1220, 1260), random.uniform(798, 803),
+                                    interval=random.uniform(0.2, 0.9), duration=random.uniform(0.2, 0.9))  # for collect
 
     def parade(self):
         count = 0
@@ -52,10 +69,10 @@ class Clicker:
                 time.sleep(1)
                 try:
                     x, y = pyautogui.locateCenterOnScreen("paradaorfeu.png")
-                    generate=random.uniform(0,5)
+                    generate = random.uniform(0, 5)
                     time.sleep(generate)
                     print(generate)
-                    pyautogui.leftClick(x, y + 30, duration=random.uniform(0, 2))
+                    pyautogui.leftClick(x, y, duration=random.uniform(0, 2))
                     count += 1
                     print("Parade date: ", count)
                     label.config(text=f"PARADE DATE:{str(count)}")
@@ -69,18 +86,19 @@ t1 = Thread(target=clicker.run)
 t2 = Thread(target=clicker.parade)
 t1.start()
 t2.start()
-
+my_progress = ttk.Progressbar(root, orient=HORIZONTAL, length=600, mode='determinate')
+my_progress.pack()
 canvas = tk.Canvas(root, height=700, width=700, bg="#000000")
 canvas.pack()
 
-b1 = tkinter.Button(root, text="Start", command=clicker.alive_set)
+b1 = tkinter.Button(root, text="Start farm", command=clicker.alive_set)
 b1.pack()
-b2 = tk.Button(root, text="Stopped", command=clicker.alive_clean)
+b2 = tk.Button(root, text="Stop farm", command=clicker.alive_clean)
 b2.pack()
 b3 = tkinter.Button(root, text="Start parada cu orfeul", command=clicker.alive_parade)
 b3.pack()
 b4 = tk.Button(root, text="Stop parada cu orfeu", command=clicker.alive_cleanparade)
 b4.pack()
-label=tk.Label(root,text="PARADE DATE: ",fg="dark green",background="black",font="bold")
+label = tk.Label(root, text="PARADE DATE: ", fg="dark green", background="black", font="bold", padx=10, pady=10)
 label.pack()
 root.mainloop()
